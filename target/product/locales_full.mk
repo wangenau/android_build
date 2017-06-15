cf91d8231755ccc609e1f413b4c401ad822975d3
wangenau <wangenau@users.noreply.github.com>
2017-06-15 20:33:19 +0200

    Remove some more languages

--- target/product/locales_full.mk
+++ target/product/locales_full.mk
@@ -1,6 +1,3 @@
 PRODUCT_LOCALES := en_US en_GB fr_FR it_IT de_DE es_ES
 
-# CyanogenMod
-PRODUCT_LOCALES += es_XA lb_LU ku_IQ ug_CN
-
 $(call inherit-product, build/target/product/languages_small.mk)
